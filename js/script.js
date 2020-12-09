fa47cb9ebb22a4284c8fc77c8bc9bf2e84f51a4d
Adityaprava Sen <adityapravasen0911@gmail.com>
2020-12-10 01:17:07 +0530

    login page and form

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ const progressCheck = document.querySelectorAll(".step .check");
 const bullet = document.querySelectorAll(".step .bullet");
 let current = 1;
 
-nextBtnFirst.addEventListener("click", function(event){
+nextBtnFirst.addEventListener("click", function (event) {
   event.preventDefault();
   slidePage.style.marginLeft = "-25%";
   bullet[current - 1].classList.add("active");
@@ -19,7 +19,7 @@ nextBtnFirst.addEventListener("click", function(event){
   progressText[current - 1].classList.add("active");
   current += 1;
 });
-nextBtnSec.addEventListener("click", function(event){
+nextBtnSec.addEventListener("click", function (event) {
   event.preventDefault();
   slidePage.style.marginLeft = "-50%";
   bullet[current - 1].classList.add("active");
@@ -27,7 +27,7 @@ nextBtnSec.addEventListener("click", function(event){
   progressText[current - 1].classList.add("active");
   current += 1;
 });
-nextBtnThird.addEventListener("click", function(event){
+nextBtnThird.addEventListener("click", function (event) {
   event.preventDefault();
   slidePage.style.marginLeft = "-75%";
   bullet[current - 1].classList.add("active");
@@ -35,18 +35,18 @@ nextBtnThird.addEventListener("click", function(event){
   progressText[current - 1].classList.add("active");
   current += 1;
 });
-submitBtn.addEventListener("click", function(){
+submitBtn.addEventListener("click", function () {
   bullet[current - 1].classList.add("active");
   progressCheck[current - 1].classList.add("active");
   progressText[current - 1].classList.add("active");
   current += 1;
-  setTimeout(function(){
+  setTimeout(function () {
     alert("Your Form Successfully Signed up");
     location.reload();
-  },800);
+  }, 800);
 });
 
-prevBtnSec.addEventListener("click", function(event){
+prevBtnSec.addEventListener("click", function (event) {
   event.preventDefault();
   slidePage.style.marginLeft = "0%";
   bullet[current - 2].classList.remove("active");
@@ -54,7 +54,7 @@ prevBtnSec.addEventListener("click", function(event){
   progressText[current - 2].classList.remove("active");
   current -= 1;
 });
-prevBtnThird.addEventListener("click", function(event){
+prevBtnThird.addEventListener("click", function (event) {
   event.preventDefault();
   slidePage.style.marginLeft = "-25%";
   bullet[current - 2].classList.remove("active");
@@ -62,7 +62,7 @@ prevBtnThird.addEventListener("click", function(event){
   progressText[current - 2].classList.remove("active");
   current -= 1;
 });
-prevBtnFourth.addEventListener("click", function(event){
+prevBtnFourth.addEventListener("click", function (event) {
   event.preventDefault();
   slidePage.style.marginLeft = "-50%";
   bullet[current - 2].classList.remove("active");
@@ -70,3 +70,76 @@ prevBtnFourth.addEventListener("click", function(event){
   progressText[current - 2].classList.remove("active");
   current -= 1;
 });
+
+//form subdivisions
+let division = document.getElementById('division');
+let edu = document.getElementById('edu');
+let dept = document.getElementById('department');
+
+division.onclick = function () {
+  if (division.value == 'state' || division.value == 'outreach') {
+    document.getElementById('state').classList.remove('hidden');
+
+    document.getElementById('society').classList.add('hidden');
+    document.getElementById('initiative').classList.add('hidden');
+  }
+  else if (division.value == 'society') {
+    document.getElementById('society').classList.remove('hidden');
+
+    document.getElementById('state').classList.add('hidden');
+    document.getElementById('initiative').classList.add('hidden');
+  }
+  else if (division.value == 'initiative') {
+    document.getElementById('initiative').classList.remove('hidden');
+
+    document.getElementById('society').classList.add('hidden');
+    document.getElementById('state').classList.add('hidden');
+  }
+  else if (division.value == 'national') {
+    document.getElementById('initiative').classList.add('hidden');
+    document.getElementById('society').classList.add('hidden');
+    document.getElementById('state').classList.add('hidden');
+  }
+}
+
+edu.onclick = function () {
+  if (edu.value == 'school' || edu.value == 'college') {
+    document.getElementById('edu-level').classList.remove('hidden');
+  }
+  else {
+    document.getElementById('edu-level').classList.add('hidden');
+  }
+}
+
+dept.onclick = function () {
+  if (dept.value == 'marketing') {
+    document.getElementById('marketing').classList.remove('hidden');
+    document.getElementById('it').classList.add('hidden');
+    document.getElementById('pub-rel').classList.add('hidden');
+    document.getElementById('finance').classList.add('hidden');
+  }
+  else if (dept.value == 'it') {
+    document.getElementById('marketing').classList.add('hidden');
+    document.getElementById('it').classList.remove('hidden');
+    document.getElementById('pub-rel').classList.add('hidden');
+    document.getElementById('finance').classList.add('hidden');
+  }
+  else if (dept.value == 'pub-rel') {
+    document.getElementById('marketing').classList.add('hidden');
+    document.getElementById('it').classList.add('hidden');
+    document.getElementById('pub-rel').classList.remove('hidden');
+    document.getElementById('finance').classList.add('hidden');
+  }
+  else if (dept.value == 'finance') {
+    document.getElementById('marketing').classList.add('hidden');
+    document.getElementById('it').classList.add('hidden');
+    document.getElementById('pub-rel').classList.add('hidden');
+    document.getElementById('finance').classList.remove('hidden');
+  }
+  else {
+    document.getElementById('marketing').classList.add('hidden');
+    document.getElementById('it').classList.add('hidden');
+    document.getElementById('pub-rel').classList.add('hidden');
+    document.getElementById('finance').classList.add('hidden');
+  }
+}
